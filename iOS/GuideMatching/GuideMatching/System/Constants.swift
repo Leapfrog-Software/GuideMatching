01981a043ca594344014050e6be950d5a934182a
藤田 祥一 <lfrogs.developer@gmail.com>
2018-06-27 11:43:24 +0900

    [iOS] コミット漏れファイルを追加

---
+++ iOS/GuideMatching/GuideMatching/System/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  GuideMatching
+//
+//  Created by Leapfrog-Software on 2018/06/27.
+//  Copyright © 2018年 Leapfrog-Inc. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let ServerRootUrl = "http://localhost/tourguide/"
+    static let ServerApiUrl = Constants.ServerRootUrl + "srv.php"
+    static let StringEncoding = String.Encoding.utf8
+    static let HttpTimeOutInterval = TimeInterval(10)
+}
