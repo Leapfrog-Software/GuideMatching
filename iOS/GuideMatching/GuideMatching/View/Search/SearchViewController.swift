03395ca542bc88628b6fa9dcd6d9f1e003c855e0
Leapfrog-Software <lfrogs.developer@gmail.com>
2018-07-23 17:43:52 +0900

    [iOS] ガイド検索ロジックを修正

--- iOS/GuideMatching/GuideMatching/View/Search/SearchViewController.swift
+++ iOS/GuideMatching/GuideMatching/View/Search/SearchViewController.swift
@@ -29,16 +29,56 @@ class SearchViewController: UIViewController {
     
     @IBOutlet private weak var languageTextField: UITextField!
     @IBOutlet private weak var keywordTextField: UITextField!
-    @IBOutlet private weak var dayTextField: UITextField!
-    @IBOutlet private weak var timeTextField: UITextField!
+    @IBOutlet private weak var dayLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var nationalityTextField: UITextField!
     @IBOutlet private weak var categoryTextField: UITextField!
     @IBOutlet private weak var orderLabel: UILabel!
 
+    private var selectedDay: Date?
+    private var selectedTime: Int?
     private var selectedOrderType = OrderType.login
     
     @IBAction func didEndEdit(_ sender: Any) {
         self.view.endEditing(true)
+    }
+    
+    @IBAction func onTapDay(_ sender: Any) {
+
+        var dates = [Date]()
+        for i in 0..<28 {
+            dates.append(Date().add(day: i))
+        }
+        let dateFormatter = DateFormatter(dateFormat: "M/d(E)")
+        let dataArray = dates.map { dateFormatter.string(from: $0) }
+        var defaultIndex = 0
+        if let day = self.selectedDay {
+            if let index = dataArray.index(of: dateFormatter.string(from: day)) {
+                defaultIndex = index
+            }
+        }
+        let picker = self.viewController(storyboard: "Common", identifier: "PickerViewController") as! PickerViewController
+        picker.set(title: "Day", dataArray: dataArray, defaultIndex: defaultIndex, completion: { [weak self] index in
+            self?.dayLabel.text = dataArray[index]
+            self?.selectedDay = dates[index]
+        })
+        self.tabbarViewController()?.stack(viewController: picker, animationType: .none)
+    }
+    
+    @IBAction func onTapTime(_ sender: Any) {
+
+        var times = [Int]()
+        for i in 0..<48 {
+            times.append(i)
+        }
+        let dataArray = times.map { String(format: "%02d", Int($0 / 2)) + ":" + String(format: "%02d", 30 * Int($0 % 2)) }
+        let defaultIndex = self.selectedTime ?? 0
+        let picker = self.viewController(storyboard: "Common", identifier: "PickerViewController") as! PickerViewController
+        picker.set(title: "Time", dataArray: dataArray, defaultIndex: defaultIndex, completion: { [weak self] index in
+            self?.timeLabel.text = dataArray[index]
+            self?.selectedTime = index
+        })
+        self.tabbarViewController()?.stack(viewController: picker, animationType: .none)
     }
     
     @IBAction func onTapOrder(_ sender: Any) {
@@ -57,14 +97,49 @@ class SearchViewController: UIViewController {
         
         var guides = GuideRequester.shared.dataList.filter { guideData -> Bool in
             let language = self.languageTextField.text ?? ""
-            if guideData.language.contains(language) {
-                return true
+            if language.count > 0 && !guideData.language.contains(language) {
+                return false
             }
+            
+            let keyword = self.keywordTextField.text ?? ""
+            if keyword.count > 0 {
+                if !guideData.email.contains(keyword)
+                    && !guideData.name.contains(keyword)
+                    && !guideData.nationality.contains(keyword)
+                    && !guideData.category.contains(keyword)
+                    && !guideData.specialty.contains(keyword)
+                    && !guideData.notes.contains(keyword) {
+                    return false
+                }
+            }
+            
+            if let day = self.selectedDay {
+                if let schedules = (guideData.schedules.filter { $0.date.isSameDay(with: day) }).first {
+                    if !schedules.isFreeList.contains(true) {
+                        return false
+                    }
+                } else {
+                    return false
+                }
+            }
+            
+            if let time = self.selectedTime {
+                let isFreeList = guideData.schedules.map { $0.isFreeList[time] }
+                if isFreeList.isEmpty {
+                    return false
+                }
+            }
+            
+            let nationality = self.nationalityTextField.text ?? ""
+            if nationality.count > 0 && !guideData.nationality.contains(nationality) {
+                return false
+            }
+            
             let category = self.categoryTextField.text ?? ""
-            if guideData.category.contains(category) {
-                return true
+            if category.count > 0 && !guideData.category.contains(category) {
+                return false
             }
-            return false
+            return true
         }
         
         guides.sort(by: { guide1, guide2 in
@@ -79,11 +154,14 @@ class SearchViewController: UIViewController {
     @IBAction func onTapReset(_ sender: Any) {
         self.languageTextField.text = ""
         self.keywordTextField.text = ""
-        self.dayTextField.text = ""
-        self.timeTextField.text = ""
+        self.dayLabel.text = ""
+        self.timeLabel.text = ""
         self.nationalityTextField.text = ""
         self.categoryTextField.text = ""
         self.orderLabel.text = OrderType.login.toString()
+        
+        self.selectedDay = nil
+        self.selectedTime = nil
         self.selectedOrderType = .login
     }
 }
