cba0e7d1f878487872495638f0f63b1c6b77a5f4
default@email.com <default@email.com>
2018-08-05 12:40:03 +0900

    [iOS] 支払い履歴画面を実装

---
+++ iOS/GuideMatching/GuideMatching/View/MyPage/Payment/MyPagePaymentTotalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyPagePaymentTotalTableViewCell.swift
+//  GuideMatching
+//
+//  Created by Leapfrog-Software on 2018/08/05.
+//  Copyright © 2018年 Leapfrog-Inc. All rights reserved.
+//
+
+import UIKit
+
+class MyPagePaymentTotalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- iOS/GuideMatching/GuideMatching/View/MyPage/Payment/MyPagePaymentViewController.swift
+++ iOS/GuideMatching/GuideMatching/View/MyPage/Payment/MyPagePaymentViewController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class MyPagePaymentViewController: UIViewController {
 
-    // TODO
+    @IBOutlet private weak var tableView: UITableView!
+    
+    func reloadTable() {
+        self.tableView.reloadData()
+    }
     
     @IBAction func onTapBack(_ sender: Any) {
         self.pop(animationType: .horizontal)
     }
 
+}
+
+extension MyPagePaymentViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyPagePaymentTotalTableViewCell", for: indexPath) as! MyPagePaymentTotalTableViewCell
+            return cell
+        } else if indexPath.row == 1 {
+            // TODO
+            return tableView.dequeueReusableCell(withIdentifier: "MyPagePaymentNoHistoryTableViewCell", for: indexPath)
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyPagePaymentAccountTableViewCell", for: indexPath) as! MyPagePaymentAccountTableViewCell
+            cell.configure(didTapEdit: {
+                
+            })
+            return cell
+        }
+    }
 }
