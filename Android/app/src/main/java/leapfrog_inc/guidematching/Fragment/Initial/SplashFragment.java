af27fe6995e011d4bdf9105c738f7bba6fb97ca1
default@email.com <default@email.com>
2018-08-08 18:40:10 +0900

    [Android] 画像追加

--- Android/app/src/main/java/leapfrog_inc/guidematching/Fragment/Initial/SplashFragment.java
+++ Android/app/src/main/java/leapfrog_inc/guidematching/Fragment/Initial/SplashFragment.java
@@ -1,6 +1,28 @@
 package leapfrog_inc.guidematching.Fragment.Initial;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import leapfrog_inc.guidematching.Fragment.BaseFragment;
+import leapfrog_inc.guidematching.R;
 
 public class SplashFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+
+        View view = inflater.inflate(R.layout.fragment_splash, null);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 1000);
+
+        return view;
+    }
 }
