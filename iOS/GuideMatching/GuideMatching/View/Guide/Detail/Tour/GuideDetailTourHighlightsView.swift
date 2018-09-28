b7c56d08ea362331ad9179d84bd4ddfd82d9f570
Leapfrog-Software <lfrogs.developer@gmail.com>
2018-09-28 18:53:06 +0900

    [iOS] ツアーハイライト画像を常に横一杯に変更

--- iOS/GuideMatching/GuideMatching/View/Guide/Detail/Tour/GuideDetailTourHighlightsView.swift
+++ iOS/GuideMatching/GuideMatching/View/Guide/Detail/Tour/GuideDetailTourHighlightsView.swift
@@ -12,6 +12,7 @@ class GuideDetailTourHighlightsView: UIView {
 
     @IBOutlet private weak var highlightsTitleLabel: UILabel!
     @IBOutlet private weak var highlightsImageView: UIImageView!
+    @IBOutlet private weak var highlightsImageHeightConstraint: NSLayoutConstraint!
     @IBOutlet private weak var bodyLabel: UILabel!
 
     private var titleString: String!
@@ -28,7 +29,11 @@ class GuideDetailTourHighlightsView: UIView {
         super.didMoveToSuperview()
         
         self.highlightsTitleLabel.text = self.titleString
-        ImageStorage.shared.fetch(url: self.imageUrl, imageView: self.highlightsImageView, defaultImage: UIImage(named: "no_image"))
+        ImageStorage.shared.fetch(url: self.imageUrl, imageView: self.highlightsImageView, defaultImage: UIImage(named: "no_image"), completion: {
+            if let image = self.highlightsImageView.image {
+                self.highlightsImageHeightConstraint.constant = (UIScreen.main.bounds.size.width - 40) * image.size.height / image.size.width
+            }
+        })
         self.bodyLabel.text = self.bodyString
     }
 }
