a51e5c55405e2d3e62aacb548bc192daf0a6edaf
default@email.com <default@email.com>
2018-09-30 01:32:22 +0900

    [iOS] メッセージ一覧の仕様変更

--- iOS/GuideMatching/GuideMatching/View/Message/MessageTableViewCell.swift
+++ iOS/GuideMatching/GuideMatching/View/Message/MessageTableViewCell.swift
@@ -13,23 +13,23 @@ class MessageTableViewCell: UITableViewCell {
     @IBOutlet private weak var faceImageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var messageLabel: UILabel!
-    @IBOutlet private weak var loginLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
 
-    func configure(guideData: GuideData) {
+    func configure(guideId: String, guideName: String, message: String, latestDate: Date) {
         
-        ImageStorage.shared.fetch(url: Constants.ServerGuideImageRootUrl + guideData.id + "-0", imageView: self.faceImageView)
+        ImageStorage.shared.fetch(url: Constants.ServerGuideImageRootUrl + guideId + "-0", imageView: self.faceImageView)
         
-        self.nameLabel.text = guideData.name
-        self.messageLabel.text = guideData.message
-        self.loginLabel.text = DateFormatter(dateFormat: "MM/dd").string(from: guideData.loginDate)
+        self.nameLabel.text = guideName
+        self.messageLabel.text = message
+        self.dateLabel.text = DateFormatter(dateFormat: "MM/dd").string(from: latestDate)
     }
     
-    func configure(guestData: GuestData) {
+    func configure(guestId: String, guestName: String, message: String, latestDate: Date) {
         
-        ImageStorage.shared.fetch(url: Constants.ServerGuestImageRootUrl + guestData.id + "-0", imageView: self.faceImageView)
+        ImageStorage.shared.fetch(url: Constants.ServerGuestImageRootUrl + guestId + "-0", imageView: self.faceImageView)
         
-        self.nameLabel.text = guestData.name
-        self.messageLabel.text = ""
-        self.loginLabel.text = ""
+        self.nameLabel.text = guestName
+        self.messageLabel.text = message
+        self.dateLabel.text = DateFormatter(dateFormat: "MM/dd").string(from: latestDate)
     }
 }
--- iOS/GuideMatching/GuideMatching/View/Message/MessageViewController.swift
+++ iOS/GuideMatching/GuideMatching/View/Message/MessageViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class MessageViewController: UIViewController {
+    
+    struct CellData {
+        let userData: Any
+        let latestMessage: String
+        let latestDate: Date
+    }
 
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var noDataLabel: UILabel!
     
-    private var userIds = [String]()
+    private var cellDatas = [CellData]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,40 +51,69 @@ class MessageViewController: UIViewController {
                         userIds.append(targetId)
                     }
                 }
-                self.userIds = userIds
-                
+                self.cellDatas = userIds.compactMap { userId -> CellData? in
+                    guard let latestMessageData = self.latestMessageData(userId: userId) else {
+                        return nil
+                    }
+                    if let guideData = GuideRequester.shared.query(id: userId) {
+                        return CellData(userData: guideData, latestMessage: latestMessageData.message, latestDate: latestMessageData.datetime)
+                    } else if let guestData = GuestRequester.shared.query(id: userId) {
+                        return CellData(userData: guestData, latestMessage: latestMessageData.message, latestDate: latestMessageData.datetime)
+                    } else {
+                        return nil
+                    }
+                }
                 self.tableView.reloadData()
-                self.noDataLabel.isHidden = !self.userIds.isEmpty
+                self.noDataLabel.isHidden = !self.cellDatas.isEmpty
             }
         })
+    }
+    
+    private func latestMessageData(userId: String) -> MessageData? {
+        
+        let filterred = MessageRequester.shared.dataList.filter { messageData -> Bool in
+            return messageData.senderId == userId || messageData.receiverId == userId
+        }
+        let sorted = filterred.sorted(by: { (message1, message2) -> Bool in
+            return message1.datetime > message2.datetime
+        })
+        return sorted.last
     }
 }
 
 extension MessageViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.userIds.count
+        return self.cellDatas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MessageTableViewCell", for: indexPath) as! MessageTableViewCell
-        let userId = self.userIds[indexPath.row]
-
-        if userId.contains("guide_") {
-            if let guideData = (GuideRequester.shared.dataList.filter { $0.id == userId }).first {
-                cell.configure(guideData: guideData)
-            }
-        } else {
-            if let guestData = (GuestRequester.shared.dataList.filter { $0.id == userId }).first {
-                cell.configure(guestData: guestData)
-            }
+        let cellData = self.cellDatas[indexPath.row]
+        
+        if let guideData = cellData.userData as? GuideData {
+            cell.configure(guideId: guideData.id, guideName: guideData.name, message: cellData.latestMessage, latestDate: cellData.latestDate)
+        } else if let guestData = cellData.userData as? GuestData {
+            cell.configure(guestId: guestData.id, guestName: guestData.name, message: cellData.latestMessage, latestDate: cellData.latestDate)
         }
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cellData = self.cellDatas[indexPath.row]
+
+        var targetUserId = ""
+        if let guideData = cellData.userData as? GuideData {
+            targetUserId = guideData.id
+        } else if let guestData = cellData.userData as? GuestData {
+            targetUserId = guestData.id
+        } else {
+            return
+        }
+        
         let detail = self.viewController(storyboard: "Message", identifier: "MessageDetailViewController") as! MessageDetailViewController
-        detail.set(targetUserId: self.userIds[indexPath.row])
+        detail.set(targetUserId: targetUserId)
         self.tabbarViewController()?.stack(viewController: detail, animationType: .horizontal)
     }
 }
